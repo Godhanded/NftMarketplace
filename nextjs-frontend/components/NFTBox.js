16b62b72c6bb2d084ceeddba793ff40a20d813f0
Godand <aezeigwe2003@gmail.com>
2023-03-01 23:42:18 +0000

    add truncate address

--- nextjs-frontend/components/NFTBox.js
+++ nextjs-frontend/components/NFTBox.js
@@ -1,14 +1,25 @@
 import { useEffect, useState } from "react";
-import { useContractRead } from "wagmi";
+import { useContractRead,useAccount } from "wagmi";
 import nftMarketplaceAbi from "../constants/NftMarketplace.json"
 import nftAbi from "../constants/MockNft.json"
 import Card from "./Card"
 import Image from "next/image"
 import { ethers } from "ethers";
 
+const truncateStr=(fullStr,strLen)=>{
+    if (fullStr.length <= strLen) return fullStr
+
+    const seperator="..."
+    const charsToShow= strLen - seperator.length
+    const frontChars= Math.ceil(charsToShow/2)
+    const backChars = Math.floor(charsToShow/2)
+    return fullStr.subString(0,frontChars)+seperator+fullStr.subString(fullStr.length-backChars)
+}
+
+
 export default function NFTBox({price,nftAddress,seller,tokenId}){
     const [imageURI,setImageURI]=useState("")
-    const [tokendata,setTokenData] =useState({name:"",description:""})
+    const [tokenData,setTokenData] =useState({name:"",description:""})
     const getUri=useContractRead({
         abi:nftAbi,
         address:nftAddress,
@@ -16,8 +27,10 @@ export default function NFTBox({price,nftAddress,seller,tokenId}){
         args:[tokenId],
         enabled:false
     })
+
+    const {isConnected,address}=useAccount()
     async function updateUI(){
-        const tokenURI= await getUri().data
+        const tokenURI= getUri?.data
         if(tokenURI){
             // ipfs gate way ipfs not supported browser
             const requestUrl= tokenURI.replace("ipfs://","https://ipfs.io/ipfs/")
@@ -29,19 +42,24 @@ export default function NFTBox({price,nftAddress,seller,tokenId}){
         }
     }
     console.log("boxed..")
-    // useEffect(()=>{
-    //     updateUI()
-    // },[])
+    useEffect(()=>{
+        updateUI()
+    },[isConnected])
+
+
+    const isOwnedByUser= seller===address ||seller==undefined
+    const fromatedSellerAddress= isOwnedByUser? "you": truncateStr(seller || "",15)
 
     return (
         
             <div>
-                {!imageURI? <Card tokenId={tokenId} title={tokendata.name} description={tokendata.description} price={`${ethers.utils.formatUnits(price,"ether")} ETH`}>
-                    <div className="italic text-sm">Owned by {seller}</div>
+                {!imageURI? <Card tokenId={tokenId} title={tokenData.name} description={tokenData.description} price={`${ethers.utils.formatUnits(price,"ether")} ETH`}>
+                    <div className="italic text-sm">Owned by {fromatedSellerAddress}</div>
                     <Image
                     className="mx-auto"
                     loader={()=>imageURI}
                     src={imageURI}
+                    alt={tokenData.name}
                     height="200"
                     width="200"
                     />
