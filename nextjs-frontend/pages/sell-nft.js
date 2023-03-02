6ccc9ff4b23e67a8bcb83d6de554678be03a8541
Godand <aezeigwe2003@gmail.com>
2023-03-02 23:04:33 +0000

    sell nft function

--- nextjs-frontend/pages/sell-nft.js
+++ nextjs-frontend/pages/sell-nft.js
@@ -1,10 +1,25 @@
-import { Form } from "@web3uikit/core"
+import { Form,useNotification } from "@web3uikit/core"
 import nftMarketplaceAbi from "../constants/NftMarketplace.json"
 import nftMarketAddress from "../constants/contractAddresses.json"
 import nftAbi from "../constants/MockNft.json"
+import { useContractWrite,usePrepareContractWrite } from "wagmi"
 import { ethers } from "ethers"
 
 export default function Home() {
+const dispatch=useNotification()
+    function handleApproveSuccess(nftAddress,tokenId,price){
+        const {write}=useContractWrite({
+            mode: 'recklesslyUnprepared',
+            abi:nftMarketplaceAbi,
+            address:nftMarketAddress[5]["NftMarketplace"][0],
+            functionName:"listItem",
+            args:[nftAddress,tokenId,price],
+    
+            onSuccess(tx){handleListSuccess()}
+        })
+        write?.()
+    }
+
 async function approveAndList(data){
     const nftAddress=data.data[0].inputResult
     const tokenId=data.data[1].inputResult
@@ -15,10 +30,23 @@ async function approveAndList(data){
         address:nftAddress,
         functionName:"approve",
         args:[nftMarketAddress[5]["NftMarketplace"][0],tokenId],
-        enabled:Boolean(tokenId)
     }
+    const {write,writeAsync}=useContractWrite({
+        mode: 'recklesslyUnprepared',
+        ...approveOptions,
+        onSuccess(tx){ handleApproveSuccess(nftAddress,tokenId,price)}
+    })
+    
 }
 
+function handleListSuccess(){
+    dispatch({
+        type:"success",
+        title:"NFT Listed",
+        message:"NFT Listing",
+        position:"topR"
+    })
+}
     return (
         <div>
             
